534ee24860df034f64dd5b03b2539a224581b959
Devil499-cell <ad0955593@gmail.com>
2026-09-07 23:11:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -370,7 +370,7 @@ def build_ui():
         gr.Markdown(
             "---\n"
             "<div class='footer'>"
-            "👨‍💻 **Developer:** @kzr0x  |  📢 **Channel:** @api_wallah"
+            "👨‍💻 **Developer:** @SOCIALBANNERR  |  📢 **Channel:** @modxpatel"
             "</div>",
             elem_classes="footer"
         )
